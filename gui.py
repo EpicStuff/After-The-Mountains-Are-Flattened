b9be677f706a0b9b929ba7db66cb05e2cc4c43c0
EpicStuff <EpicStuff@users.noreply.github.com>
2023-06-27 10:59:42 -0400

    working on stage 0: switching away from .bind

--- gui.py
+++ gui.py
@@ -1,29 +1,56 @@
 import asyncio
-from typing import Callable
 from nicegui import app, ui
+from stuff import Binder, Dict, wrap
+
 class GUI():
-	def __init__(self) -> None:
-		# setup storage
-		self.data = app.storage.user
-		if 'stage' not in self.data: self.data['stage'] = 0
-		# setup ui elements
-		self.elms = type('Holder', (), {
-			'pop': ui.dialog()
-		})()
-		with self.elms.pop, ui.card():
-			self.elms.pop.text = ui.label('')
-			self.elms.pop.but = ui.button('', on_click=self.elms.pop.close)
-	def dialog(self, text: str, but: str) -> None:
-		self.elms.pop.text.set_text(text)
-		self.elms.pop.but.set_text(but)
-		self.elms.pop.open()
-	def stage1(self) -> None:
+	def __init__(i, vars: dict, init: dict) -> None:
+		from main import get_level, get_xp, handle_key
+		# setup "variables"
+		i.vars = Dict(vars)
+		i.user = Binder(app.storage.user)
+		i.user.update(init, overwrite=False)
+
+		# setup ui element holder
+		i.elms = Dict({
+			'key': ui.keyboard(on_key=wrap(handle_key, i))
+		})
+		# add maxed out xp bar and label
 		with ui.row().classes('w-full'):
-			self.elms.level = ui.label('Level:')
-			with ui.linear_progress(self.data['xp'], size='20px', show_value=False).props('instant-feedback').classes('w-1/2') as self.elms.xp:
-				self.elms.xp.text = ui.label(self.data['level']).classes('absolute-center text-sm text-white')
+			with ui.column().classes('w-1/2'):
+				i.elms.level = ui.label()
+				with ui.linear_progress(size='20px', show_value=False).props('instant-feedback').classes('w-1/2').bind_value_from(i.user._target, 'xp', wrap(get_xp, i.vars.xp_showing)) as i.elms.xp:
+					i.elms.xp.text = ui.label().classes('absolute-center text-sm text-white').bind_text_from(i.user._target, 'xp', wrap(get_level, i.vars.xp_showing))
+			with ui.column():
+				ui.label().bind_text_from(i.user._target, 'xp', wrap(get_level, i.vars.xp_showing))
+				ui.label().bind_text_from(i.user._target, 'xp', wrap(get_xp, i.vars.xp_showing))
+		# setup popup
+		with ui.dialog().props('persistent') as i.elms.pop, ui.card():
+			i.elms.pop.text = ui.label('')
+			i.elms.pop.but = ui.button('', on_click=i.elms.pop.close)
+	def set_stage_0(i) -> None:
+		i.elms.level.set_text('Saana Online:')
+		i.elms.xp.text.set_text('999')
+	def open_popup(i, text: str, but: str) -> None:
+		'sets the text of label and button then opens the popup'
+		i.elms.pop.text.set_text(text)
+		i.elms.pop.but.set_text(but)
+		i.elms.pop.open()
+	def set_stage_1(i) -> None:
+		i.elms.level.set_text('Literature:')
+		i.elms.xp.text.set_text('')
+		i.vars.xp_showing = 'l'
+	def set_stage_2(i) -> None:
+		pass
 
 
-async def delay(func: Callable, secs: int) -> None:
-	await asyncio.sleep(secs)
-	func()
+# def tmp(i, num):
+# 	print('-------------------------------------------------------')
+# 	print(num)
+# 	print(i.vars.xp_showing)
+# 	print(num[i.vars.xp_showing] % 1)
+# 	print('-------------------------------------------------------')
+# 	return (num[i.vars.xp_showing] - i.vars.zero) % 1
+
+# def tmp2(i, num):
+# 	print(num, i.vars.tmp, int(num[i.vars.tmp]))
+# 	return int(num[i.vars.tmp])
--- main.py
+++ main.py
@@ -1,58 +1,74 @@
-import mountains as m, math
-from gui import GUI, app, ui
-from stuff import wrap
+import mountains as m
+from gui import GUI, ui
+from stuff import e, wrap, KeyEventArguments
 
-consts = {
-	'tick_speed': .01,
-	'xp': lambda level: math.e ** level * 100000 / 10000,
-	'levels': {},
+vars = {
+	'tick_speed': 1e-3,
+	# 'tick_speed': 1,  # for debugging
+	'max_xp': lambda i, level: e ** level * 1e6 / 1e3,
+	'levels': {
+		's': {999: 1e9},
+	},
+	'xp_showing': 's',
+	# 'zero': 1e-9,
+}
+default = {
+	'stage': 0,
+	'xp': {
+		's': 999.999,
+		'l': 1 / 10,
+		'm': 0,
+	},
 }
 
-@ui.page('/')
-async def main() -> None:
-	gui = GUI()
-	if gui.data['stage'] == 0: await stage0(gui)
-	if gui.data['stage'] == 1: await stage1(gui)
-
-async def stage0(gui) -> None:
-	gui.dialog(m.story0, 'Okay')
-	await gui.elms.pop.but.clicked()
-	gui.data['stage'] = 1
-	gui.data['xp'] = gui.data['level'] = 0
-async def stage1(gui) -> None:
-	gui.stage1()
-	ui.timer(consts['tick_speed'], wrap(gain_xp, gui))
-def gain_xp(gui) -> None:
-	# if xp required to level up has not been calculated, calculate
-	if gui.data['level'] not in consts['levels']:
-		consts['levels'][gui.data['level']] = consts['xp'](gui.data['level'])
-	gui.data['xp'] += 1 / consts['levels'][gui.data['level']]
-	# if should level up
-	if gui.data['xp'] > 1:
-		gui.data['level'] += 1
-		gui.data['xp'] = 0
-		gui.elms.xp.text.set_text(gui.data['level'])
-	gui.elms.xp.set_value(gui.data['xp'])
-
-	# gui.data['xp'] += 1
-	# if gui.data['xp'] > 100:
-	# 	# gui.data['level'] += 1
-	# 	gui.data['xp'] = 0
-	# 	gui.elms.xp.text.set_text(gui.data['level'])
-	# tmp = gui.data['xp'] / 100
-	# gui.elms.xp.set_value(gui.data['xp'] / 100)
-
-
-if __name__ in {"__main__", "__mp_main__"}:
-	import tracemalloc, glob, json, os
-	tracemalloc.start()
-
-	for file in glob.glob('.nicegui/*.json'):
+def json_filter(folder='.') -> None:
+	import glob, json, os
+	for file in glob.glob(folder + '*.json'):
 		try:
 			with open(file, 'r') as f:
 				json.load(f)
 		except json.decoder.JSONDecodeError:
 			os.remove(file)
+def get_level(xp: dict, stage: 'str') -> int | str:
+	return int(xp[stage]) if int(xp[stage]) != 0 else ''
+def get_xp(stage: 'str', xp: dict) -> float:
+	return xp[stage] % 1
+def gain_xp(stage: 'str', i: GUI) -> None:
+	'increment xp by 1 / xp required to level up'
+	level = get_level(i.user.xp, stage)
+	# if xp required to level up has not been calculated, calculate
+	if level not in i.vars.levels[stage]:
+		i.vars.levels[stage][level] = i.vars.max_xp[stage](level)
+	# increment xp
+	i.user.xp[stage] += 1 / i.vars.levels[stage][level]
+def handle_key(i: GUI, e: KeyEventArguments):
+	if e.modifiers.ctrl and e.action.repeat and e.key == 'c':
+		print('resetting progress')
+		i.user.update(default)
+@ui.page('/')
+async def main() -> None:
+	i = GUI(vars, default)
 
-	ui.run(dark=True, storage_secret='test')
+	i.set_stage_0()
+	if i.user.stage == 0: await stage_0(i)
+
+	i.set_stage_1()
+	if i.user.stage == 1: await stage_1(i)
+
+async def stage_0(i: GUI) -> None:
+	# open popup
+	i.open_popup(m.story_0, 'Okay?')
+	# wait for button to be clicked
+	await i.elms.pop.but.clicked()
+	i.user.stage = 1
+	i.user.level = ''
+	i.user.xp = 1 / 5
+async def stage_1(i: GUI) -> None:
+	i.elms.ticker = ui.timer(i.vars.tick_speed, wrap(gain_xp, i, i.vars.xp_showing))
+
+
+if __name__ in {"__main__", "__mp_main__"}:
+	# import tracemalloc; tracemalloc.start()
+	json_filter('.nicegui/')
+	ui.run(dark=True, storage_secret='secret')
 	ui.open(main)
