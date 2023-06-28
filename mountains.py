94891eb0bf7d28c7771ed1935bcaaeff3e6c136c
EpicStuff <EpicStuff@users.noreply.github.com>
2023-06-28 15:26:50 -0400

    updated story 0

--- mountains.py
+++ mountains.py
@@ -1,2 +1,2 @@
 # story0 = 'Having surmounted the tallest mountain, the peak of literature. The climber proceeds to the next hill, a minor side hobby. The 1 v 1'
-story_0 = 'Having finally surmounted the mountain that was Saana Online, the climber may now finally retire at the age of 17 to pursue what really matters. The climb that is the all-encompassing, all-synthesising exploration of the world of literatu—'
+story_0 = 'Having finally surmounted the mountain that was Saana Online, the climber may now finally retire at the age of 17 to pursue what really matters. The climb that is the all-encompassing, all-synthesizing exploration of the world of literatu—'
