0ab773834c5583269ab8fecf6576a1c1c68cd222
EpicStuff <EpicStuff@users.noreply.github.com>
2023-07-03 18:06:59 -0400

    added quests and tried to fix newline (or lack thereof)

--- mountains.py
+++ mountains.py
@@ -1,31 +1,38 @@
 story_0 = 'Having finally surmounted the mountain that was Saana Online, the climber may now finally retire at the age of 17 to pursue what really matters. The climb that is the all-encompassing, all-synthesizing exploration of the world of literatu—'
 events = {
 	'The Card': {
-		'title': 'The Card',
+		'title': 'The Card:',
 		'text': '''
-			"holy crap just agree already!!!! what's taking so long!! here’s the deal: if u get 1st in the 1v1, i won’t pull that card again."
-			The card, the climber's attention honed in on this.
-			The card!
-			What was the card? Long ago the frenemy had helped his family out in a massive way when the climber's mother had fallen ill. Since then, it'd been reminders of this favour that'd motivated the climber to put up with the frenemy's nonsense.
-			In a way, the card was the last thing binding them together. Once it was removed, it'd really be over...the climber would be retired for good.
-			So that was tonight's wager: first place in his own recruitment tournament in exchange for eternal freedom, the warm bliss that lay beyond the mountain's frosty slopes.
-			The climber, leaned back to contemplate the offer while, across from him, his long-time comrade feigned indifference sawing through a piece of pork.
-			The Card...
-			The Card...the Car—
+			"holy crap just agree already!!!! what's taking so long!! here’s the deal: if u get 1st in the 1v1, i won’t pull that card again."\n
+			The card, the climber's attention honed in on this.\n
+			The card!\n
+			What was the card? Long ago the frenemy had helped his family out in a massive way when the climber's mother had fallen ill. Since then, it'd been reminders of this favour that'd motivated the climber to put up with the frenemy's nonsense.\n
+			In a way, the card was the last thing binding them together. Once it was removed, it'd really be over...the climber would be retired for good.\n
+			So that was tonight's wager: first place in his own recruitment tournament in exchange for eternal freedom, the warm bliss that lay beyond the mountain's frosty slopes.\n
+			The climber, leaned back to contemplate the offer while, across from him, his long-time comrade feigned indifference sawing through a piece of pork.\n
+			The Card...\n
+			The Card...the Car—\n
 		''',
 		'buttons': ['It was only two weeks, Deal!', 'Isnt\'t this underhanded scheming too much?'],
 	},
 }
+quests = {
+	'The Card': {
+		'title': 'Accept The Challenge:',
+		'text': 'The Climbers frenemy has proposed a challenge. Accept it so the story my continue.',
+		'buttons': ['Fine', 'Not Doing'],
+	},
+}
 insights = {
 	'Wu Wei': {
-		'title': 'Wu Wei',
+		'title': 'Wu Wei:',
 		'text': '''
-			What had the climber ever done to deserve this treatment? Had it really been too much to ask for a 17-year-old man to retire quietly in peace? Had he not put in enough work for one lifetime?
-			It seemed so silly to him.
-			Recently, in the spirit of retirement, he'd been doing much reading about his new non-vocation. The greatest insights had come from the bearded philosophers of ancient China, who'd captured the essence of quitting perfectly with the principle of Wu-Wei, or non-doing.
-			Non-doing, one might argue that this was the secret spice to all human advancement. Just as the life of an individual required offsetting its more active, doing parts with more restful, non-doing parts to maintain a functional balance, so too might this greater life which we call civilization require offsetting its more active, doing people with more restful, non-doing people. Man was a creature of comparison, and without the low achiever, could anyone truly be a high achiever? Behind every great man of action, the climber would assert there was another man—much less great but no less important—a man of inaction, a man whose inert, flabby example terrified the great man and drove him to strive on to ever-higher altitudes. And who were we, the non-doers, to strip this great man of his vital motivation? Ask yourself, if the next Einstein walked through the door right now, would you deny him his potential? Henry could never be so selfish. For the greater good of society, he would leap in front of the lazy bullet. He would sacrifice himself by becoming one of those men who dare, boldly, to do nothing. As for his great man, he could be any one of the souls brimming with untapped potential around him - he could be the waiter, he could be an anonymous diner, and, why of course, he could even be you.
-
-			Effect: tick speed multiplier
+			What had the climber ever done to deserve this treatment? Had it really been too much to ask for a 17-year-old man to retire quietly in peace? Had he not put in enough work for one lifetime?\n
+			It seemed so silly to him.\n
+			Recently, in the spirit of retirement, he'd been doing much reading about his new non-vocation. The greatest insights had come from the bearded philosophers of ancient China, who'd captured the essence of quitting perfectly with the principle of Wu-Wei, or non-doing.\n
+			Non-doing, one might argue that this was the secret spice to all human advancement. Just as the life of an individual required offsetting its more active, doing parts with more restful, non-doing parts to maintain a functional balance, so too might this greater life which we call civilization require offsetting its more active, doing people with more restful, non-doing people. Man was a creature of comparison, and without the low achiever, could anyone truly be a high achiever? Behind every great man of action, the climber would assert there was another man—much less great but no less important—a man of inaction, a man whose inert, flabby example terrified the great man and drove him to strive on to ever-higher altitudes. And who were we, the non-doers, to strip this great man of his vital motivation? Ask yourself, if the next Einstein walked through the door right now, would you deny him his potential? Henry could never be so selfish. For the greater good of society, he would leap in front of the lazy bullet. He would sacrifice himself by becoming one of those men who dare, boldly, to do nothing. As for his great man, he could be any one of the souls brimming with untapped potential around him - he could be the waiter, he could be an anonymous diner, and, why of course, he could even be you.\n
+\n
+			Effect: tick speed multiplier\n
 		''',
 	},
 }
