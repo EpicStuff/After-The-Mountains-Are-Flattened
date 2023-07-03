967b88b12ee0a3ac1d7da91c9c6eee9e9e4ca77e
EpicStuff <EpicStuff@users.noreply.github.com>
2023-07-02 22:08:38 -0400

    added first event

--- mountains.py
+++ mountains.py
@@ -1,2 +1,18 @@
-# story0 = 'Having surmounted the tallest mountain, the peak of literature. The climber proceeds to the next hill, a minor side hobby. The 1 v 1'
 story_0 = 'Having finally surmounted the mountain that was Saana Online, the climber may now finally retire at the age of 17 to pursue what really matters. The climb that is the all-encompassing, all-synthesizing exploration of the world of literatu—'
+events = {
+	'The Card': {
+		'title': 'The Card',
+		'text': '''
+			"holy crap just agree already!!!! what's taking so long!! here’s the deal: if u get 1st in the 1v1, i won’t pull that card again."
+			The card,
+			The card!
+			What is the card? Long ago you've been helped out in a massive way. Since then, it'd been the reminders of this favour that'd motivated you to put up with all this nonsense.
+			In a way, this card is the last thing binding you. Once removed, it'd really be over...you'd be retired for good.
+			So that's the wager: first place in the minor tournament in exchange for eternal freedom, the warm bliss that lay beyond the mountain's frosty slopes.
+			you lean back to contemplate the offer.
+			The Card...
+			The Card...the Car—
+		''',
+		'buttons': ['What the heck, Deal!', 'No Way, had you not done enough?'],
+	},
+}
