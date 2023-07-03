c5f795be0822bd0f587e767a6d0161463bb2ef08
EpicStuff <EpicStuff@users.noreply.github.com>
2023-07-02 22:09:00 -0400

    added first insight

--- mountains.py
+++ mountains.py
@@ -16,3 +16,16 @@ events = {
 		'buttons': ['What the heck, Deal!', 'No Way, had you not done enough?'],
 	},
 }
+insights = {
+	'Wu Wei': {
+		'title': 'Wu Wei',
+		'text': '''
+			What had the climber ever done to deserve this treatment? Had it really been too much to ask for a 17-year-old man to retire quietly in peace? Had he not put in enough work for one lifetime?
+			It seemed so silly to him.
+			Recently, in the spirit of retirement, he'd been doing much reading about his new non-vocation. The greatest insights had come from the bearded philosophers of ancient China, who'd captured the essence of quitting perfectly with the principle of Wu-Wei, or non-doing.
+			Non-doing, one might argue that this was the secret spice to all human advancement. Just as the life of an individual required offsetting its more active, doing parts with more restful, non-doing parts to maintain a functional balance, so too might this greater life which we call civilization require offsetting its more active, doing people with more restful, non-doing people. Man was a creature of comparison, and without the low achiever, could anyone truly be a high achiever? Behind every great man of action, the climber would assert there was another man—much less great but no less important—a man of inaction, a man whose inert, flabby example terrified the great man and drove him to strive on to ever-higher altitudes. And who were we, the non-doers, to strip this great man of his vital motivation? Ask yourself, if the next Einstein walked through the door right now, would you deny him his potential? Henry could never be so selfish. For the greater good of society, he would leap in front of the lazy bullet. He would sacrifice himself by becoming one of those men who dare, boldly, to do nothing. As for his great man, he could be any one of the souls brimming with untapped potential around him - he could be the waiter, he could be an anonymous diner, and, why of course, he could even be you.
+
+			Effect: tick speed multiplier
+		''',
+	},
+}
